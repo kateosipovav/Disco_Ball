a0d6cd02249997fe2a683d7b62c1d2fafb2c6f7e
Kate Osipova <kateosipovav@gmail.com>
2020-02-02 20:20:47 -0800

    First Commit

---
+++ Disco Ball/src/DiscoBall.java
@@ -0,0 +1,42 @@
+import java.lang.Math;
+public class DiscoBall{
+	 private static double dip = 1.5; 
+ public static double getPlantVolume(){
+   //volume of the cubicle minus the volume of the
+   //inner cube
+	
+   double bVol = Math.pow(Cubicle.SIDE - dip, 3);
+   double sVol = Math.pow(Plant.HEIGHT, 3);
+   return (bVol-sVol);
+ }
+public static double getSurfaceArea(){
+   return 6*Math.pow(Cubicle.SIDE, 2);
+ }
+public static double getMaxPlantNum(){
+int volumeMaxNum = (int) (getPlantVolume() / Plant.VOLUME);
+    int areaMaxNum = (int) (getSurfaceArea() / Plant.BASESURFACE);
+ 
+    if (volumeMaxNum <= areaMaxNum) {
+      System.out.println("The volume is the limiting factor");
+      System.out.println("The number of plants this time is " + volumeMaxNum);
+      System.out.println("__________________________________________________");
+      System.out.println();
+      System.out.println();
+      return volumeMaxNum;
+    }
+ 
+    else {
+      System.out.println("The area is the limiting factor");
+      System.out.println("The number of plants this time is " + areaMaxNum);
+      System.out.println("__________________________________________________");
+      System.out.println();
+      System.out.println();
+      return areaMaxNum;
+    }
+  }
+public static void run(){
+	System.out.println("The maximum number of plants this system can fit is " + getMaxPlantNum());
+}
+ }
+
+
---
+++ Disco Ball/src/Simulation.java
@@ -0,0 +1,8 @@
+
+public class Simulation {
+
+	public static void main(String[] args) {
+        DiscoBall.run();
+
+	}
+}
